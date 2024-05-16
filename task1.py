55a178cc5d0a6c3af7dee4f22457e4c383cb5e7e
Michael <michaelmagued31@gmail.com>
2024-05-16 22:55:00 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -17,7 +17,7 @@ def packet_callback(packet): # Called for each packet captured by sniffer
 print("Starting network sniffer... \n")
 try:
     # Put in "iface=" the name of my network interface: Ethernet or Wi-Fi !!!
-    sniff(prn=packet_callback, iface='Ethernet', store=0, count=10)
+    sniff(prn=packet_callback, iface='Ethernet', store=0, count=100)
    # Initiates the packet sniffing process. It uses the sniff function from Scapy, specifying packet_callback as the callback function to handle captured packets. The iface parameter specifies the network interface to sniff on, in this case, it's set to 'Wi-Fi'. The store parameter is set to 0, which means it doesn't store packets in memory, and the count parameter is set to 10, which specifies the number of packets to sniff.
 
 except KeyboardInterrupt: # Catches the KeyboardInterrupt exception, which is raised when the user interrupts the script (e.g., by pressing Ctrl+C)
