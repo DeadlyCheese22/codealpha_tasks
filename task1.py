585d3a4da421db3b3f6d81d37cb40cff4aac2ae7
Michael <michaelmagued31@gmail.com>
2024-05-22 22:17:45 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 from scapy.all import sniff, IP, TCP
 
 def packet_callback(packet): # Called for each packet captured by sniffer
-    if packet.haslayer(IP):
+    if packet.haslayer(IP): # Checks if captured packet has an IP layer
         ip_src = packet[IP].src # Extracts source IP address from IP layer of packet
         ip_dst = packet[IP].dst # Extracts  destination IP address from IP layer of packet
         protocol = packet[IP].proto # Extracts protocol number from IP layer of packet, indicating the protocol used (ex: TCP, UDP...)
@@ -19,7 +19,7 @@ try:
     # Put in "iface=" the name of my network interface: Ethernet or Wi-Fi !!!
     sniff(prn=packet_callback, iface='Ethernet', store=0, count=100)
    # Initiates the packet sniffing process. It uses the sniff function from Scapy, specifying packet_callback as the callback function to handle captured packets. The iface parameter specifies the network interface to sniff on, in this case, it's set to 'Ethernet'. 
-   # The store parameter is set to 0, which means it doesn't store packets in memory, and the count parameter is set to 10, which specifies the number of packets to sniff.
+   # The store parameter is set to 0, which means it doesn't store packets in memory, and the count parameter is the number of packets to sniff.
    
 except KeyboardInterrupt: # Catches the KeyboardInterrupt exception, which is raised when the user interrupts the script (e.g., by pressing Ctrl+C)
     print("\nStopping network sniffer...") #Prints a message indicating that the network sniffer is stopping
